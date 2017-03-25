86c28bb68d40d1e53503a57c81b2b0f47fa6c00b
RYArrowsmith <ryarrowsmith@protonmail.com>
2017-03-25 11:38:04 +0000

    Softban import changes.

--- src/main/java/io/bfnt/comportment/diax/commands/moderation/Softban.java
+++ src/main/java/io/bfnt/comportment/diax/commands/moderation/Softban.java
@@ -1,6 +1,9 @@
 package io.bfnt.comportment.diax.commands.moderation;
 
-import io.bfnt.comportment.diax.api.command.*;
+import io.bfnt.comportment.diax.api.command.CommandDescription;
+import io.bfnt.comportment.diax.api.command.ErrorType;
+import io.bfnt.comportment.diax.api.command.ModerationCommand;
+import io.bfnt.comportment.diax.api.command.Punishment;
 import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.Message;
