442f4589c89cbbbc2d8b92d722c18b44ef24a2c5
RYArrowsmith <ryarrowsmith@protonmail.com>
2017-03-25 15:35:10 +0000

    Softban fix

--- src/main/java/io/bfnt/comportment/diax/commands/moderation/Softban.java
+++ src/main/java/io/bfnt/comportment/diax/commands/moderation/Softban.java
@@ -1,8 +1,8 @@
 package io.bfnt.comportment.diax.commands.moderation;
 
 import io.bfnt.comportment.diax.api.command.CommandDescription;
+import io.bfnt.comportment.diax.api.command.DiaxCommand;
 import io.bfnt.comportment.diax.api.command.ErrorType;
-import io.bfnt.comportment.diax.api.command.ModerationCommand;
 import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.Message;
@@ -12,20 +12,18 @@ import net.dv8tion.jda.core.entities.Message;
  * Dev'ving like a sir since 1998. | https://github.com/Comportment
  */
 @CommandDescription(guildOnly = true, minimumArgs = 1, permission = Permission.KICK_MEMBERS, name = "softban", emoji = "🔨", args = "@mention")
-public class Softban extends ModerationCommand
+public class Softban extends DiaxCommand
 {
     public void execute(Message trigger)
     {
-        try
-        {
-            Member member = getMemberFromString(trigger.getRawContent().split(" ")[1], trigger.getGuild());
-            trigger.getGuild().getController().ban(member, 7).queue(_void ->
-                    trigger.getGuild().getController().unban(member.getUser()).queue(_void_ ->
-                        trigger.getChannel().sendMessage(makeMessage("Softbanned!", getNiceName(member) + " has been softbanned.").build()).queue()));
-        }
-        catch (NullPointerException e)
+        Member member = getMemberFromString(trigger.getRawContent().split(" ")[1], trigger.getGuild());
+        if (member == null)
         {
             makeError(trigger.getChannel(), ErrorType.USER_NOT_FOUND);
+            return;
         }
+        trigger.getGuild().getController().ban(member, 7).queue(_void ->
+                trigger.getGuild().getController().unban(member.getUser()).queue(__void ->
+                        trigger.getChannel().sendMessage(makeMessage("Softbanned!", getNiceName(member) + " has been softbanned.").build()).queue()));
     }
 }
