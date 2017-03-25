786df0f92680ccd99c1dbe1b9eb477b970c1e3ab
Comportment <Comportment@users.noreply.github.com>
2017-03-25 19:59:55 +0000

    Actual bump message

--- src/main/java/io/bfnt/comportment/diax/commands/Bump.java
+++ src/main/java/io/bfnt/comportment/diax/commands/Bump.java
@@ -3,6 +3,8 @@ package io.bfnt.comportment.diax.commands;
 import io.bfnt.comportment.diax.api.BumpTimer;
 import io.bfnt.comportment.diax.api.command.CommandDescription;
 import io.bfnt.comportment.diax.api.command.DiaxCommand;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Message;
 
 /**
@@ -12,19 +14,19 @@ import net.dv8tion.jda.core.entities.Message;
 @CommandDescription(name = "bump", guildOnly = true, emoji = "🎉")
 public class Bump extends DiaxCommand
 {
-    private int cooldown = 10;
     public void execute(Message trigger)
     {
+        int coolDown = 10;
         if (BumpTimer.getBumps().containsKey(trigger.getGuild()))
         {
             long timeLeft = (trigger.getCreationTime().toEpochSecond() - BumpTimer.getBumps().get(trigger.getGuild()));
-            if (timeLeft >= cooldown)
+            if (timeLeft >= coolDown)
             {
                 bump(trigger);
             }
             else
             {
-                trigger.getChannel().sendMessage(makeMessage("Error", "Please wait " + (cooldown - timeLeft) + " more seconds more to do this.").build()).queue();
+                trigger.getChannel().sendMessage(makeMessage("Error", "Please wait " + (coolDown - timeLeft) + " more seconds more to do this.").build()).queue();
             }
         }
         else
@@ -34,7 +36,10 @@ public class Bump extends DiaxCommand
     }
     private void bump(Message trigger)
     {
-        BumpTimer.removeBump(trigger.getGuild());
+        Guild guild = trigger.getGuild();
+        String invite = "https://discord.gg/" + trigger.getGuild().getPublicChannel().createInvite().complete();
+        BumpTimer.removeBump(guild);
+        trigger.getJDA().getGuildById("293889712014360586").getTextChannelById("294519934996971520").sendMessage(new EmbedBuilder().setAuthor(guild.getName(), invite, guild.getIconUrl()).setDescription(String.format("Join %s by using this link: %s", guild.getName(), invite)).setColor(trigger.getGuild().getMember(trigger.getAuthor()).getColor()).build()).queue();
         trigger.getChannel().sendMessage(makeMessage("Bumped!", trigger.getGuild().getName() + " has been bumped!").build()).queue();
         BumpTimer.addBump(trigger.getGuild(), trigger.getCreationTime().toEpochSecond());
     }
