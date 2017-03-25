21d26e801b943fd7d9a47b950fa8d0379de4d094
Comportment <Comportment@users.noreply.github.com>
2017-03-25 19:38:22 +0000

    bump(trigger);

--- src/main/java/io/bfnt/comportment/diax/commands/Bump.java
+++ src/main/java/io/bfnt/comportment/diax/commands/Bump.java
@@ -12,18 +12,29 @@ import net.dv8tion.jda.core.entities.Message;
 @CommandDescription(name = "bump", guildOnly = true, emoji = "🎉")
 public class Bump extends DiaxCommand
 {
+    private int cooldown = 5;
     public void execute(Message trigger)
     {
         if (BumpTimer.getBumps().containsKey(trigger.getGuild()))
         {
             long timeleft = (trigger.getCreationTime().toEpochSecond() - BumpTimer.getBumps().get(trigger.getGuild()));
-            if (timeleft >= 5) BumpTimer.removeBump(trigger.getGuild());
-            trigger.getChannel().sendMessage("" + timeleft).queue();
+            if (timeleft >= cooldown)
+            {
+                bump(trigger);
+            }
+            else
+            {
+                trigger.getChannel().sendMessage(makeMessage(" ", "Please wait " + cooldown + " more seconds more to do this.").build()).queue();
+            }
         }
         else
         {
-            trigger.getChannel().sendMessage("not in the timer").queue();
-            BumpTimer.addBump(trigger.getGuild(), trigger.getCreationTime().toEpochSecond());
+            bump(trigger);
         }
+    }
+    private void bump(Message trigger)
+    {
+        BumpTimer.removeBump(trigger.getGuild());
+        trigger.getChannel().sendMessage(makeMessage("Bumped!", trigger.getChannel().getName() + " has been bumped!").build()).queue();
     }
 }
