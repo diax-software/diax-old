2643801ec63adf466ad9e2dec87e90a7279d2f1b
RYArrowsmith <ryarrowsmith@protonmail.com>
2017-03-25 15:31:25 +0000

    Null checking

--- src/main/java/io/bfnt/comportment/diax/commands/moderation/Ban.java
+++ src/main/java/io/bfnt/comportment/diax/commands/moderation/Ban.java
@@ -1,6 +1,7 @@
 package io.bfnt.comportment.diax.commands.moderation;
 
 import io.bfnt.comportment.diax.api.command.CommandDescription;
+import io.bfnt.comportment.diax.api.command.ErrorType;
 import io.bfnt.comportment.diax.api.command.ModerationCommand;
 import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.Member;
@@ -16,6 +17,11 @@ public class Ban extends ModerationCommand
     public void execute(Message trigger)
     {
         Member member = getMemberFromString(trigger.getRawContent().split(" ")[1], trigger.getGuild());
+        if (member == null)
+        {
+            makeError(trigger.getChannel(), ErrorType.USER_NOT_FOUND);
+            return;
+        }
         trigger.getGuild().getController().ban(member, 7).queue(_void ->
                 trigger.getChannel().sendMessage(makeMessage("Banned!", getNiceName(member) + " has been banned.").build()).queue());
 
