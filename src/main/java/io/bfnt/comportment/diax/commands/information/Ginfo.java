b88055883e25568c326cb917b2ae43c345cd30ca
Comportment <Comportment@users.noreply.github.com>
2017-03-25 17:05:29 +0000

    M E M E S

--- src/main/java/io/bfnt/comportment/diax/commands/information/Ginfo.java
+++ src/main/java/io/bfnt/comportment/diax/commands/information/Ginfo.java
@@ -4,6 +4,10 @@ import io.bfnt.comportment.diax.api.command.CommandDescription;
 import io.bfnt.comportment.diax.api.command.DiaxCommand;
 import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.Role;
+
+import java.time.temporal.ChronoUnit;
+import java.util.stream.Collectors;
 
 /**
  * Created by Comporment on 25/03/2017 at 16:38
@@ -15,6 +19,6 @@ public class Ginfo extends DiaxCommand
     public void execute(Message trigger)
     {
         Guild guild = trigger.getTextChannel().getGuild();
-        trigger.getChannel().sendMessage(makeMessage("About: " + guild.getName(), "").build()).queue();
+        trigger.getChannel().sendMessage(makeMessage(guild.getName(), String.format("🙇 Owner: %s\n🌍 Region: %s\n🎧 Voice Channels: %d\n🖊 Text Channels: %d\n📓 Total Channels: %d\n👥 Members: %d\n👤Roles: (%d)\n%s\n🕛 Created: %s day(s) ago.", guild.getOwner().getUser().getName() + "#" + guild.getOwner().getUser().getDiscriminator(), guild.getRegion().getName(), guild.getVoiceChannels().size(), guild.getTextChannels().size(), guild.getVoiceChannels().size() + guild.getTextChannels().size(), guild.getMembers().size(), guild.getRoles().size(), guild.getRoles().stream().map(Role::getName).collect(Collectors.joining(", ")).replaceFirst(", @everyone", "."), guild.getCreationTime().until(trigger.getCreationTime(), ChronoUnit.DAYS))).build()).queue();
     }
 }
