0c029e3821e302f4569d87836673d5c9a3667f71
RYArrowsmith <ryarrowsmith@protonmail.com>
2017-03-25 15:33:11 +0000

    Kick fix

--- src/main/java/io/bfnt/comportment/diax/commands/moderation/Kick.java
+++ src/main/java/io/bfnt/comportment/diax/commands/moderation/Kick.java
@@ -1,8 +1,8 @@
 package io.bfnt.comportment.diax.commands.moderation;
 
 import io.bfnt.comportment.diax.api.command.CommandDescription;
+import io.bfnt.comportment.diax.api.command.DiaxCommand;
 import io.bfnt.comportment.diax.api.command.ErrorType;
-import io.bfnt.comportment.diax.api.command.ModerationCommand;
 import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.Message;
@@ -12,19 +12,17 @@ import net.dv8tion.jda.core.entities.Message;
  * Dev'ving like a sir since 1998. | https://github.com/Comportment
  */
 @CommandDescription(name = "kick", args = "@mention", emoji = "👞", permission = Permission.KICK_MEMBERS, minimumArgs = 1, guildOnly = true)
-public class Kick extends ModerationCommand
+public class Kick extends DiaxCommand
 {
     public void execute(Message trigger)
     {
-        try
-        {
-            Member member = getMemberFromString(trigger.getRawContent().split(" ")[1], trigger.getGuild());
-            trigger.getGuild().getController().ban(member, 7).queue(_void ->
-                    trigger.getChannel().sendMessage(makeMessage("Kicked!", getNiceName(member) + " has been kicked.").build()).queue());
-        }
-        catch (NullPointerException e)
+        Member member = getMemberFromString(trigger.getRawContent().split(" ")[1], trigger.getGuild());
+        if (member == null)
         {
             makeError(trigger.getChannel(), ErrorType.USER_NOT_FOUND);
+            return;
         }
+        trigger.getGuild().getController().kick(member).queue(_void ->
+                trigger.getChannel().sendMessage(makeMessage("Kicked!", getNiceName(member) + " has been kicked.").build()).queue());
     }
 }
