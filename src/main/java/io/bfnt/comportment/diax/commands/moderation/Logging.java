973bb8ee1140b0c5d63e297ad503c700d9419298
Comportment <Comportment@users.noreply.github.com>
2017-03-26 20:04:56 +0100

    logback

--- src/main/java/io/bfnt/comportment/diax/commands/moderation/Logging.java
+++
@@ -1,9 +0,0 @@
-package io.bfnt.comportment.diax.commands.moderation;
-
-/**
- * Created by Comporment on 26/03/2017 at 19:43
- * Dev'ving like a sir since 1998. | https://github.com/Comportment
- */
-public class Logging {
-
-}
