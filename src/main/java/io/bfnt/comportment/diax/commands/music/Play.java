e777b02366b99cf0ca09bf248187210751a325fd
Comportment <Comportment@users.noreply.github.com>
2017-03-26 14:14:41 +0100

    Update info

--- src/main/java/io/bfnt/comportment/diax/commands/music/Play.java
+++ src/main/java/io/bfnt/comportment/diax/commands/music/Play.java
@@ -9,7 +9,7 @@ import net.dv8tion.jda.core.entities.Message;
  * Created by Comporment on 26/03/2017 at 11:41
  * Dev'ving like a sir since 1998. | https://github.com/Comportment
  */
-@CommandDescription(minimumArgs = 1, name = "play", guildOnly = true, permission = Permission.VOICE_MOVE_OTHERS)
+@CommandDescription(minimumArgs = 1, name = "play", guildOnly = true, permission = Permission.VOICE_MOVE_OTHERS, args = "YouTube URL", emoji = "🎧")
 public class Play extends DiaxCommand
 {
     public void execute(Message trigger)
