fcb7776c8a2ca8e8ea5781a6a98e142f5c502eca
Comportment <Comportment@users.noreply.github.com>
2017-03-28 14:53:01 +0100

    Logging and stuff

--- src/main/java/io/bfnt/comportment/diax/Main.java
+++ src/main/java/io/bfnt/comportment/diax/Main.java
@@ -5,6 +5,7 @@ import io.bfnt.comportment.diax.lib.Token;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.Game;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
 import java.util.Scanner;
@@ -37,15 +38,21 @@ public final class Main extends Diax
     {
         for (int i = 0; i < shards; i++)
         {
-            System.out.println("Starting shard: " + i);
-            JDA jda = new JDABuilder(AccountType.BOT).setToken(Token.mainToken()).useSharding(i, shards + i).addListener(new Main()).buildAsync();
-            System.out.println("Shard " + i + " started successfully.");
+            staticLog(String.format("Shard %d is starting.", i));
+            JDA jda = new JDABuilder(AccountType.BOT).setToken(Token.mainToken()).useSharding(i, shards).setGame(Game.of(String.format("%shelp | Shard %s/%s", staticPrefix(), i, shards - 1))).addListener(new Main()).buildAsync();
+            staticLog(String.format("Shard %d has been started.", i));
         }
     }
 
+    /**
+     * Test command to tell which shard Diax is on.
+     *
+     * @param event Method fired when bot receives a message.
+     * @since Azote
+     */
     @Override
     public void onMessageReceived(MessageReceivedEvent event)
     {
-        if (event.getMessage().getRawContent().startsWith("<<")) event.getChannel().sendMessage(event.getJDA().getShardInfo().getShardId() + " shard.").queue();
+        if (event.getMessage().getRawContent().startsWith("<<")) event.getChannel().sendMessage("Shard ID: " + event.getJDA().getShardInfo().getShardId() + "/" + (event.getJDA().getShardInfo().getShardTotal() - 1)).queue();
     }
 }
--- src/main/java/io/bfnt/comportment/diax/lib/Diax.java
+++ src/main/java/io/bfnt/comportment/diax/lib/Diax.java
@@ -2,11 +2,58 @@ package io.bfnt.comportment.diax.lib;
 
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by Comporment on 28/03/2017 at 12:09
  * Dev'ving like a sir since 1998. | https://github.com/Comportment
  */
-public class Diax extends ListenerAdapter {
+public class Diax extends ListenerAdapter
+{
+    /**
+     * Logging from within static methods.
+     *
+     * @param message Message to timestamp and then print to console.
+     * @since Azote
+     */
+    protected static void staticLog(String message)
+    {
+        System.out.println(new SimpleDateFormat("[HH:mm:ss] ").format(new Date()) + message);
+    }
 
+    /**
+     * Logging from within normal methods.
+     *
+     * @param message Message to timestamp and then print to console.
+     * @since Azote
+     * @deprecated
+     */
+    @Deprecated
+    protected void log(String message)
+    {
+        System.out.println(new SimpleDateFormat("[HH:mm:ss] ").format(new Date()) + message);
+    }
 
+    /**
+     * Getting a static version of the prefix.
+     *
+     * @since Azote
+     */
+    protected static String staticPrefix()
+    {
+        return "<<";
+    }
+
+    /**
+     * Getting the prefix.
+     *
+     * @since Azote
+     * @deprecated
+     */
+    @Deprecated
+    protected String getPrefix()
+    {
+        return "<<";
+    }
 }
