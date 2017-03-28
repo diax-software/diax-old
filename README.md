93adfcbc01498489b602b625631a7168a85661fe
Comportment <Comportment@users.noreply.github.com>
2017-03-28 11:21:06 +0100

    logback

--- README.md
+++ README.md
@@ -1,4 +1,40 @@
-# Diax
-Diax bot for Discord using the JDA wrapper.
-
-Will come to replace Relash soon :3
+<!--TODO: Make this look pretty-->
+<div align = "center">
+<h1>Diax-0.0.3</h1>
+<hr>
+<a href = "https://bfnt.io/"><img src = "https://img.shields.io/badge/Supported_By-Blackfields_Network-red.svg"></a>
+<a href = "https://github.com/Comportment"><img src = "https://img.shields.io/badge/Made_By-Comportment-green.svg"></a>
+<a href = "https://github.com/Comportment"><img src = "https://img.shields.io/badge/Code_Name-Azote-blue.svg"></a>
+<a href = "https://github.com/Truency"><img src = "https://img.shields.io/badge/Inspired_By-Nomsy-lightgrey.svg"></a>
+<hr>
+</div>
+<div style="width:100%;">
+<table>
+  <tr>
+    <th>Feature Name</th>
+    <th>Completion Status</th> 
+  </tr>
+  <tr>
+    <td>Command Handler</td>
+    <td>Work in progress.</td>
+  </tr>
+  <tr>
+    <td>Music</td>
+    <td>Work in progress.</td> 
+  </tr>
+</table>
+<hr>
+<table>
+  <tr>
+    <th>Command Name</th>
+    <th>Description</th> 
+    <th>Status</th>
+  </tr>
+  <tr>
+    <td>Help</td>
+    <td>Gives you help about Diax's commands.</td>
+    <td>Not started.</td>
+  </tr>
+</table>
+</div>
+<hr>
--- src/main/java/io/bfnt/comportment/diax/Main.java
+++ src/main/java/io/bfnt/comportment/diax/Main.java
@@ -1,36 +1,8 @@
 package io.bfnt.comportment.diax;
 
-import io.bfnt.comportment.diax.api.Diax;
-import io.bfnt.comportment.diax.api.command.CommandHandler;
-import io.bfnt.comportment.diax.api.music.DisconnectListener;
-import io.bfnt.comportment.diax.token.Token;
-import net.dv8tion.jda.core.AccountType;
-import net.dv8tion.jda.core.JDA;
-import net.dv8tion.jda.core.JDABuilder;
-import net.dv8tion.jda.core.entities.Game;
-
 /**
  * Created by Comporment on 22/03/2017 at 19:09
  * Dev'ving like a sir since 1998. | https://github.com/Comportment
  */
-public final class Main extends Diax
-{
-    public static void main(String[] args)
-    {
-        try
-        {
-            JDA jda = new JDABuilder(AccountType.BOT)
-                    .setToken(Token.main())
-                    .setAudioEnabled(true)
-                    .setGame(Game.of("<>help"))
-                    .buildBlocking();
-            jda.addEventListener(new CommandHandler(), new DisconnectListener());
-        }
-        catch (Exception exception)
-        {
-            System.err.println("Someone thing wrong :/\nHave an error message so I can make it up to you <3\n");
-            exception.printStackTrace();
-            System.err.println("\nEnd of error message. Hope you fix the bug.");
-        }
-    }
+public final class Main {
 }
