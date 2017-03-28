0b7a14e151a496962f08a0fd9653e501e7d16d18
Comportment <Comportment@users.noreply.github.com>
2017-03-28 11:35:56 +0100

    Main methods

--- src/main/java/io/bfnt/comportment/diax/Main.java
+++ src/main/java/io/bfnt/comportment/diax/Main.java
@@ -4,5 +4,10 @@ package io.bfnt.comportment.diax;
  * Created by Comporment on 22/03/2017 at 19:09
  * Dev'ving like a sir since 1998. | https://github.com/Comportment
  */
-public final class Main {
+public final class Main
+{
+    public static void main(String[] args)
+    {
+
+    }
 }
