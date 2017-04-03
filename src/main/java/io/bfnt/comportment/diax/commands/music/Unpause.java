8aeb02afd4ded3ec3f41b70de52d05a4da777920
Unknown <U10411204@wigan-leigh.ac.uk>
2017-04-03 20:09:41 +0100

    Unpause, and pause

--- src/main/java/io/bfnt/comportment/diax/commands/music/Unpause.java
+++ src/main/java/io/bfnt/comportment/diax/commands/music/Unpause.java
@@ -13,16 +13,16 @@ import java.awt.*;
  * Created by Comporment on 02/04/2017 at 16:21
  * Dev'ving like a sir since 1998. | https://github.com/Comportment
  */
-@CommandDescription(triggers = {"pause"}, permission = Permission.VOICE_CONNECT, guildOnly = true, minimumArgs = 1, description = "Pauses the player")
-public class Pause extends DiaxCommand {
+@CommandDescription(triggers = {"unpause"}, permission = Permission.VOICE_CONNECT, guildOnly = true, minimumArgs = 1, description = "Pauses the player")
+public class Unpause extends DiaxCommand {
     @Override
     public void execute(Message trigger) {
         GuildMusicManager manager = MusicUtil.getMusicManager(trigger.getTextChannel());
-        if (!manager.player.isPaused()) {
-            manager.player.setPaused(true);
-            trigger.getChannel().sendMessage(makeEmbed().addField("Success!", "Music has been paused", false).build()).queue();
+        if (manager.player.isPaused()) {
+            manager.player.setPaused(false);
+            trigger.getChannel().sendMessage(makeEmbed().addField("Success!", "Music has been unpaused", false).build()).queue();
         } else {
-            trigger.getChannel().sendMessage(makeEmbed().setColor(new Color(255, 0, 0)).addField("Error", "Music is already paused", false).build()).queue();
+            trigger.getChannel().sendMessage(makeEmbed().setColor(new Color(255, 0, 0)).addField("Error", "Music is already unpasued", false).build()).queue();
         }
     }
 }
