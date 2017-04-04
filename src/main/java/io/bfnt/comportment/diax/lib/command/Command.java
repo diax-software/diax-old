5469895a556d2a120dafc888ed50ce3ad4097c76
Comportment <Comportment@users.noreply.github.com>
2017-04-04 08:02:25 +0100

    +AudioTrackContext | Breaking all commands

--- src/main/java/io/bfnt/comportment/diax/lib/command/Command.java
+++ src/main/java/io/bfnt/comportment/diax/lib/command/Command.java
@@ -12,7 +12,10 @@ public interface Command {
      * Method that is fired when a {@link DiaxCommand} is being executed.
      *
      * @param trigger The {@link Message} which triggered the command.
+     * @param truncated A String containing the message without the args.
+     * @apiNote This Method was changed in Brazen to include the truncated arg to allow for multiple prefixes.
+     * @author Comportment
      * @since Azote
      */
-    void execute(Message trigger);
+    void execute(Message trigger, String truncated);
 }
