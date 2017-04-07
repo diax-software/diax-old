7b0465da9414b80311174043dad063c448d8c414
Comportment <Comportment@protonmail.com>
2017-04-07 23:05:36 +0100

    Eval anyone?

--- diax-bot/src/main/java/io/bfnt/comportment/diax/bot/commands/owner/EvalCommand.java
+++ diax-bot/src/main/java/io/bfnt/comportment/diax/bot/commands/owner/EvalCommand.java
@@ -2,9 +2,11 @@ package io.bfnt.comportment.diax.bot.commands.owner;
 
 import io.bfnt.comportment.diax.bot.lib.command.DiaxCommand;
 import io.bfnt.comportment.diax.bot.lib.command.DiaxCommandDescription;
+import io.bfnt.comportment.diax.bot.lib.util.DiaxUtil;
 import net.dv8tion.jda.core.entities.Message;
 
 import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
 
 /**
  * Created by Comportment on 07/04/2017.
@@ -15,6 +17,22 @@ public class EvalCommand extends DiaxCommand {
 
     @Override
     public void execute(Message trigger, String truncated) {
-        ScriptEngine engine;
+        ScriptEngine engine = this.addMethods(new ScriptEngineManager().getEngineByName("nashorn"), trigger);
+        String output;
+        try {
+            output = "" + engine.eval(String.join("\n", "load('nashown:mozilla_compat.js');", "imports = new JavaImporter(java.util, java.io);", "(function(){", "with(imports){", truncated, "}", "})()"));
+        } catch (Exception exception) {
+            output = exception.getMessage();
+        }
+        trigger.getChannel().sendMessage(DiaxUtil.defaultEmbed().setDescription(output).build()).queue();
+    }
+
+    private ScriptEngine addMethods(ScriptEngine engine, Message trigger) {
+        engine.put("jda", trigger.getJDA());
+        engine.put("message", trigger);
+        engine.put("event", trigger);
+        engine.put("guild", trigger.getGuild());
+        engine.put("channel", trigger.getChannel());
+        return engine;
     }
 }
