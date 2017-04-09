1e928c3d713a2c5b8823860589c1077c5277c2ab
Comportment <Comportment@protonmail.com>
2017-04-09 11:57:48 +0100

    I should version stuff

---
+++ diax-site/src/main/java/io/bfnt/comportment/diax/web/Testing.java
@@ -0,0 +1,13 @@
+package io.bfnt.comportment.diax.web;
+
+/**
+ * Created by Comportment on 09/04/2017.
+ * If you don't understand this, we are screwed.
+ */
+public class Testing {
+    static String toTest = "memes";
+
+    public static String memes() {
+        return toTest.toUpperCase();
+    }
+}
