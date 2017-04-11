2bf61d370393f942c5e361b79be161dab64d5d59
Comportment <Comportment@protonmail.com>
2017-04-11 13:52:48 +0100

    Ginfo

--- diax-bot/src/main/java/me/diax/bot/commands/statistics/Ginfo.java
+++ diax-bot/src/main/java/me/diax/bot/commands/statistics/Ginfo.java
@@ -21,12 +21,14 @@ public class Ginfo extends DiaxCommand {
         trigger.getChannel().sendMessage(
                 DiaxUtil.defaultEmbed()
                         .setAuthor(guild.getName(), guild.getIconUrl(), guild.getIconUrl())
-                        .setDescription(String.format("\uD83D\uDE47 Owner: %s\n\uD83C\uDF0D Region: %s\n\n\uD83C\uDFA7 Voice Channels: %s\n\uD83D\uDD8A Text Channels: %s\n\uD83D\uDCD3 Total Channels: %s\n\n\uD83D\uDC65 Members: %s\n\uD83D\uDC64 Roles: %s\n\n\uD83D\uDD5B Created: %s days ago.",
+                        .setDescription(String.format("\uD83D\uDE47 Owner: %s\n\uD83C\uDF0D Region: %s\n\n\uD83C\uDFA7 Voice Channels: %s\n\uD83D\uDD8A Text Channels: %s\n\uD83D\uDCD3 Total Channels: %s\n\n\uD83E\uDD16 Bots: %s\n\uD83D\uDC65 Users: %s\n➕ Total: %s\n\n\uD83D\uDC64Roles: %s\n\n\uD83D\uDD5B Created: %s days ago.",
                                 DiaxUtil.makeName(guild.getOwner().getUser()),
                                 guild.getRegion().getName(),
                                 guild.getVoiceChannels().size(),
                                 guild.getTextChannels().size(),
                                 (guild.getTextChannels().size() + guild.getVoiceChannels().size()),
+                                guild.getMembers().stream().filter(member -> member.getUser().isBot()).count(),
+                                guild.getMembers().stream().filter(member -> ! member.getUser().isBot()).count(),
                                 guild.getMembers().size(),
                                 guild.getRoles().size(),
                                 guild.getCreationTime().until(trigger.getCreationTime(), ChronoUnit.DAYS)))
